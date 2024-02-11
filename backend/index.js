30180b94f286158296e26965b90c9c460235825b
Candy505 <amayanegi42@gmail.com>
2024-02-11 20:13:02 +0530

    database collection added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { UserModel } from './src/models/Users.js'
 import {userRouter} from './src/routes/users.js'
 
 const app = express();
@@ -9,12 +10,28 @@ app.use(express.json());
 app.use(cors());
 
 
-const dbURL = "mongodb+srv://medhabisht525:JXO3V9Tm1pMAoWZm@cluster0.bod5wll.mongodb.net/Recipe-app"
+const dbURL = "mongodb+srv://medhabisht525:JXO3V9Tm1pMAoWZm@cluster0.bod5wll.mongodb.net/"
 
 app.use("/auth",userRouter);
 
 mongoose.connect(dbURL).then((res)=>
 {
     app.listen(3000);
-    console.log("database connected")
-}).catch((err)=> console.log(err))
+  
+
+    const newData = new UserModel({
+        name: 'John Doe',
+        password:'123'
+    });
+
+    // Save the new document to the collection
+    try {
+         newData.save();
+        console.log('New document added to the collection');
+    } catch (error) {
+        console.error('Error adding document:', error);
+    }
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+})
